82c0fd5197fa9134fbd9824456207276a28707f6
Sean <skabe89@gmail.com>
2021-09-30 10:52:15 -0400

    added test validations

--- test/reverseString.js
+++ test/reverseString.js
@@ -2,8 +2,18 @@ let reverseString = require('../reverseString.js')
 let expect = require('chai').expect
 const validator = require('../reverseString')
 
-describe('#reverseString()', function(){
-  context('reverse hello', function(){
+describe('#reverseString()', () => {
+
+  it('should reverse hello', function(){
     expect(validator.reverseString('hello')).to.equal('olleh')
   })
+
+  it("should reverse 'Goodnight Moon'", function(){
+    expect(validator.reverseString('Goodnight Moon')).to.equal('nooM thgindooG')
+  })
+
+  it("should reverse '  the   fox jumpedOver the    fence  ", () => {
+    expect(validator.reverseString('  the   fox jumpedOver the    fence  ')).to.equal('  ecnef    eht revOdepmuj xof   eht  ')
+  })
+
 })
