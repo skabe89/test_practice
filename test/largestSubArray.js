271b5de81c8256e312978ec0cdc259992e5b3d9d
Sean <skabe89@gmail.com>
2021-10-18 23:49:51 -0400

    4 total tests for largest sub array

--- test/largestSubArray.js
+++ test/largestSubArray.js
@@ -1,8 +1,21 @@
 const expect = require('chai').expect
 const validator = require('../largestSubArray')
+const testOne = [1, 2, -3, -2, 2]
+const testTwo = [2, 1, -2, 3, 2]
+const testThree = [-1]
+const testFour = [0]
 
 describe('#largestSubArray()', () => {
-  it('Should return Hello', () => {
-    expect(validator.largestSubArray('Hello')).to.equal('Hello')
+  it('Should Return largest Sub Array from Array of Nums', () => {
+    expect(validator.largestSubArray(testOne)).to.equal(3)
+  })
+  it('Should Return largest Sub Array from Array of Nums', () => {
+    expect(validator.largestSubArray(testTwo)).to.equal(6)
+  })
+  it('Should Return 0 if largest Sub Array is Negative', () => {
+    expect(validator.largestSubArray(testThree)).to.equal(0)
+  })
+  it('Should Return 0 if array is empty', () => {
+    expect(validator.largestSubArray(testFour)).to.equal(0)
   })
 })
