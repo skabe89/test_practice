eb3e629cf736188b9b377c7ee17fcbb4575f195f
Sean <skabe89@gmail.com>
2021-10-20 23:55:08 -0400

    twoSum works, test1

--- twoSum.js
+++ twoSum.js
@@ -1,3 +1,13 @@
-exports.twoSum = (str) => {
-  return str
+exports.twoSum = (arr, target) => {
+  let set = new Set()
+  for(let i = 0; i < arr.length; i++){
+    let needed = target - arr[i]
+    if(set.has(needed)){
+      return true
+    }
+    else{
+      set.add(arr[i])
+    }
+  }
+  return false
 }
