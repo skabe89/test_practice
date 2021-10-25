2cf939513e7d771fa563346dd38f67a56aa18c8d
Sean <skabe89@gmail.com>
2021-10-24 21:58:04 -0400

    added tests to bubbleSort

--- test/bubbleSort.js
+++ test/bubbleSort.js
@@ -1,9 +1,17 @@
 const expect = require('chai').expect
 const validator = require('../bubbleSort')
-const arr1 = "Hello World"
+const arr1 = [5,4,3,2,1]
+const arr2 = [1,3,2,5,4]
+const arr3 = [34,50,24,43,12]
 
 describe('#bubbleSort()', () => {
   it('should sort array ascending order', () => {
-    expect(validator.bubbleSort(arr1)).to.equal('Hello World')
+    expect(validator.bubbleSort(arr1)).to.equal([1,2,3,4,5])
+  })
+  it('should sort array ascending order', () => {
+    expect(validator.bubbleSort(arr2)).to.equal([1,2,3,4,5])
+  })
+  it('should sort array ascending order', () => {
+    expect(validator.bubbleSort(arr3)).to.equal([12,24,34,43,50])
   })
 })
